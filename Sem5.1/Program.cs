bf2641d8c4bb37694184db3cc1f0c0a837be5772
RpPrime <Nikitamatveev007@gmail.com>
2023-08-18 13:54:38 +0300

    Задача 34 готова

---
+++ Sem5.1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Функция создания массива
+int[] GetArray(int size, int firstnumber, int secondnumber)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(firstnumber, secondnumber);
+    }
+    return result;
+}
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите начальное число: ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите конечное число: ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+int[] array = GetArray(size, firstnumber, secondnumber);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+int resultCount (int[] arr)
+{
+    int count = 0;
+    foreach (int el in arr)
+    {
+        if (el % 2 == 0) count ++;
+    }
+    return count;
+}
+
+int count = resultCount(array);
+
+Console.WriteLine($"Колличество четных элементов в массиве [{string.Join(", ", array)}] равно: {count}");
