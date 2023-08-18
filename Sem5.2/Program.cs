1bb1ffdf6ac7af2d06ca022b854577e02f8b965a
RpPrime <Nikitamatveev007@gmail.com>
2023-08-18 13:55:50 +0300

    Задача 36 готова

---
+++ Sem5.2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Функция создания массива
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(10, 100);
+    }
+    return result;
+}
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = GetArray(size);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+int SumElements(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0) sum = sum + arr[i];
+    }
+    return sum;
+}
+
+int sum = SumElements(array);
+Console.WriteLine($"Сумма элементов на нечетных индексах равна: {sum}");
